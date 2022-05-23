b9b30167445cdeed531dc0073ee76fab39f2a7a2
seoyun75 <67564166+seoyun75@users.noreply.github.com>
2022-05-24 05:46:54 +0900

    header에 token data set

--- naeggeodo/src/main/java/com/naeggeodo/oauth/OAuthController.java
+++ naeggeodo/src/main/java/com/naeggeodo/oauth/OAuthController.java
@@ -1,27 +1,26 @@
 package com.naeggeodo.oauth;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.naeggeodo.jwt.JwtTokenProvider;
 import com.naeggeodo.jwt.JwtTokenService;
-import com.naeggeodo.jwt.dto.RefreshTokenRequest;
-import com.naeggeodo.oauth.config.OauthTokenResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,6 +37,7 @@ public class OAuthController {
     @RequestMapping(value = "login/OAuth/{provider}")
     public void getCode(@PathVariable String provider, HttpServletResponse response) {
     	String uri = service.getOAuthLoginUrl(provider); 
+    	
     	log.info(uri);
     	try {
 			response.sendRedirect(uri);
@@ -55,19 +55,23 @@ public class OAuthController {
 //    			jwtService.createJwtToken(service.getAuth(code, provider))));
 //    }
     
-    @RequestMapping(value = "oauth/getInfo/{provider}")
-    public String OAuthLogin(@RequestParam("code") String code, @PathVariable String provider) throws JSONException, Exception {
-    	log.info(code);
-    	return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(
-    			jwtService.createJwtToken(service.getAuth(code, provider)));
-    }
-    
-    @RequestMapping(value = "login/getToken/{provider}")
-    public OauthTokenResponse tokenTest(@RequestParam("code") String code, @PathVariable String provider) throws JsonMappingException, JsonProcessingException {
+    @GetMapping(value = "oauth/getInfo/{provider}")
+    public ResponseEntity<?> OAuthLogin(@RequestParam("code") String code, @PathVariable String provider, HttpServletResponse response) throws JSONException, Exception {
     	
-    	return service.getTokenTest(code, provider);
-    
-//    @PostMapping("/refreshtoken")
+    	MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        Map<String, String> map = new ObjectMapper().convertValue(jwtService.createJwtToken(service.getAuth(code, provider)), new TypeReference<Map<String, String>>() {}); // (3)
+        params.setAll(map); // (4)
+
+        
+    	HttpHeaders header = new HttpHeaders(params)  ;
+    	
+    	log.info(header.toString());
+    			
+    	return ResponseEntity.ok().headers(header).body("ok");
+    	
+    }
+
+    //    @PostMapping("/refreshtoken")
 //    public ResponseEntity<?> refreshtoken(@RequestBody RefreshTokenRequest request) {
 //        String requestRefreshToken = request.getRefreshToken();
 //        
@@ -76,12 +80,12 @@ public class OAuthController {
 //        }
 //      return refreshTokenService.findByToken(requestRefreshToken)
 //          .map(refreshTokenService::verifyExpiration)
-//  s        .map(RefreshToken::getUser)
+//          .map(RefreshToken::getUser)
 //          .map(user -> {
 //            String token = jwtUtils.generateTokenFromUsername(user.getUsername());
 //            return ResponseEntity.ok(new TokenRefreshResponse(token, requestRefreshToken));
 //          })
 //          .orElseThrow(() -> new TokenRefreshException(requestRefreshToken,
 //              "Refresh token is not in database!"));
-    }
+    
 }
