28cb5424993a71c9f3a1d50ccaed4f4f8b4f20c3
seoyun75 <67564166+seoyun75@users.noreply.github.com>
2022-05-26 00:32:17 +0900

    소셜로그인 인가코드 요청 로직분리

--- naeggeodo/src/main/java/com/naeggeodo/oauth/OAuthController.java
+++ naeggeodo/src/main/java/com/naeggeodo/oauth/OAuthController.java
@@ -1,18 +1,21 @@
 package com.naeggeodo.oauth;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.HeadersBuilder;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.naeggeodo.jwt.JwtTokenProvider;
 import com.naeggeodo.jwt.JwtTokenService;
+import com.naeggeodo.jwt.dto.RefreshTokenRequest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,44 +38,30 @@ public class OAuthController {
     private final JwtTokenService jwtService;
     
     //소셜로그인으로부터 인증코드를 받아와 OAuthLogin으로 redirecte
-    @RequestMapping(value = "login/OAuth/{provider}")
-    public void getCode(@PathVariable String provider, HttpServletResponse response) {
-    	String uri = service.getOAuthLoginUrl(provider); 
-    	
-    	log.info(uri);
-    	try {
-			response.sendRedirect(uri);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    @RequestMapping(value = "login/{provider}")
+    public void getCode(@PathVariable String provider, HttpServletResponse response, HttpServletRequest request) {
+    	//url에 필요한 데이터
     }
     
-//    @RequestMapping(value = "oauth/getInfo/{provider}")
-//    public ResponseEntity<?> OAuthLogin(@RequestParam("code") String code, @PathVariable String provider) throws JSONException, Exception {
-//    	log.info(code);
-//    	return ResponseEntity.ok(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(
-//    			jwtService.createJwtToken(service.getAuth(code, provider))));
-//    }
+    @GetMapping(value = "login/OAuth/{provider}")
+    public ResponseEntity<?> OAuthCallback(){
+    	return ResponseEntity.ok("ok");
+    }
     
-    @GetMapping(value = "oauth/getInfo/{provider}")
-    public ResponseEntity<?> OAuthLogin(@RequestParam("code") String code, @PathVariable String provider, HttpServletResponse response) throws JSONException, Exception {
+    @PostMapping(value = "login/OAuth/{provider}")
+    public ResponseEntity<?> OAuthLogin(@RequestParam("code") String code, @PathVariable String provider) throws JSONException, Exception {
     	
     	MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         Map<String, String> map = new ObjectMapper().convertValue(jwtService.createJwtToken(service.getAuth(code, provider)), new TypeReference<Map<String, String>>() {}); // (3)
-        params.setAll(map); // (4)
+        params.setAll(map);
 
-        
-    	HttpHeaders header = new HttpHeaders(params)  ;
+    	HttpHeaders header = new HttpHeaders(params);
     	
-    	log.info(header.toString());
-    			
     	return ResponseEntity.ok().headers(header).body("ok");
     	
     }
 
-    //    @PostMapping("/refreshtoken")
+//    @PostMapping("/refreshtoken")
 //    public ResponseEntity<?> refreshtoken(@RequestBody RefreshTokenRequest request) {
 //        String requestRefreshToken = request.getRefreshToken();
 //        
@@ -87,5 +77,6 @@ public class OAuthController {
 //          })
 //          .orElseThrow(() -> new TokenRefreshException(requestRefreshToken,
 //              "Refresh token is not in database!"));
-    
+//    
+//    }
 }
