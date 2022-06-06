8279e5906ffc8377d6c375b10eef4a67c415b9f9
김민혁 <94831670+kmh916@users.noreply.github.com>
2022-06-06 20:33:18 +0900

    Add and Modify Report

---
+++ naeggeodo/src/main/java/com/naeggeodo/controller/ReportController.java
@@ -0,0 +1,28 @@
+package com.naeggeodo.controller;
+
+import com.naeggeodo.dto.ReportDTO;
+import com.naeggeodo.entity.post.Report;
+import com.naeggeodo.entity.user.Users;
+import com.naeggeodo.repository.ReportRepository;
+import com.naeggeodo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReportController {
+    private final ReportRepository reportRepository;
+    private final UserRepository userRepository;
+
+    @PostMapping(value = "/report",produces = "application/json")
+    @Transactional
+    public String saveReport(@RequestBody ReportDTO reportDTO){
+        Users user = userRepository.getById(reportDTO.getUser_id());
+        Report report =  Report.create(user,reportDTO.getContents(),reportDTO.getType());
+        reportRepository.save(report);
+        return "asd";
+    }
+}
