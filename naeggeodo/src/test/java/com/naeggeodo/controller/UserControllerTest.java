1fae09538adb7886e9c5b77ffc0bd5ed7bb55b5d
김민혁 <94831670+kmh916@users.noreply.github.com>
2022-12-29 20:45:35 +0900

    mypage 변경을 위한 컨트롤러 테스트코드

---
+++ naeggeodo/src/test/java/com/naeggeodo/controller/UserControllerTest.java
@@ -0,0 +1,89 @@
+package com.naeggeodo.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@Sql("classpath:h2/userController.sql")
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    WebApplicationContext applicationContext;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build()
+        ;
+
+    }
+
+    @Test
+    void updateAddress() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+
+    //
+    @Test
+    void getMyPageDate() throws Exception {
+        String userId = "user0";
+
+        MvcResult mvcResult = mockMvc.perform(get("/user/{user_id}/mypage", userId))
+                .andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        JSONObject jsonObject = new JSONObject(contentAsString);
+
+        assertAll(
+                () -> assertThat(jsonObject.get("participatingChatCount")).isEqualTo(1),
+                () -> assertThat(jsonObject.get("myOrdersCount")).isEqualTo(1),
+                () -> assertThat(jsonObject.get("nickname")).isEqualTo(123)
+        );
+    }
+
+    @Test
+    void getQuickChat() {
+    }
+
+    @Test
+    void updateQuickChat() {
+    }
+
+    @Test
+    void getNickname() {
+    }
+
+    @Test
+    void updateNickname() {
+    }
+}
---
+++ naeggeodo/src/test/resources/h2/userController.sql
@@ -0,0 +1,29 @@
+delete from deal;
+delete from chat_main;
+delete from quick_chat;
+delete from users;
+
+insert into quick_chat(quick_chat_id) values(hibernate_sequence.nextval);
+
+insert into users(user_id,quick_chat_id,nickname)
+values('user0',select quick_chat_id from quick_chat where rownum() = 1,'123');
+
+insert into chat_main(chatmain_id,category,state,building_code,title,user_id,create_date)
+values(hibernate_sequence.nextval,'CHINESE','CREATE','111','1st','user0','2022-11-05T17:50:39.095');
+
+insert into chat_user(chat_user_id,user_id,chatmain_id,ban_state)
+values(hibernate_sequence.nextval,'user0',
+          select chatmain_id
+       from chat_main
+       where title = '1st',
+       'ALLOWED'
+      );
+
+insert into deal(deal_id,user_id,chatmain_id)
+values (
+        hibernate_sequence.nextval,
+        'user0',
+        select chatmain_id
+        from chat_main
+        where title = '1st'
+);
